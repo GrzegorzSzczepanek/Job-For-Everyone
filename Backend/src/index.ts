1be6ed34ab49558143a9d9a3b6b775408c36bb39
Ernest Gupik <ernestgupik@wp.pl>
2023-10-16 23:11:29 +0200

    Make CORS allow all URLs

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors')
+
 const app = express();
+
 app.use(express.json());
+app.use(cors())
 
 app.get('/api/test', (req, res) => {
     res.json({ message: 'Hello, World!' });
