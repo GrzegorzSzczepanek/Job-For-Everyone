cd89aa9448f150e37597e68ef43ed4a909ea18d4
Foxyg3n <piotrek.suchecki@vp.pl>
2023-10-18 22:48:43 +0200

    Created status endoint

--- Frontend/src/routes/status/+page.svelte
+++ Frontend/src/routes/status/+page.svelte
@@ -1,7 +1,14 @@
 <script>
     import { getData } from '$lib';
 
-    const json = getData("192.168.105.223", "ping");
+    const promise = getData("192.168.105.233", "ping");
 </script>
 
-<div>{json.status == "pong" ? "Działa backend" : "Nie działa backend"}</div>
+<!-- <div>{json.status == "pong" ? "Działa backend" : "Nie działa backend"}</div> -->
+{#await promise}
+    <p>Checking backend status...</p>
+{:then}
+    <p>OK</p>
+{:catch error}
+    <p style="color: red">{error.message}</p>
+{/await}
