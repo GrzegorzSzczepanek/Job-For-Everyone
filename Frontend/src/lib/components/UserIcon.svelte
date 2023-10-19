aba9fed4b1168a770b16c7b702c6df37e1627161
Grzesiek <grzegorzszczepanek48@gmail.com>
2023-10-19 02:59:42 +0200

    Search bar (#4)
    * search bar and generating items works
    
    * minor additions

---
+++ Frontend/src/lib/components/UserIcon.svelte
@@ -0,0 +1,32 @@
+<script>
+import { goto } from '$app/navigation';
+
+let profile_image = "https://imgs.search.brave.com/Cx1UU2_Kf_zCkw3prnIJ9TFp24dA9DWwnhn7-XzG13k/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAwLzYwLzMzLzU4/LzM2MF9GXzYwMzM1/ODg0XzdCQ25DdzVr/bTVpVFNKcXBGMzR2/czVBUjg3dTJsUkdp/LmpwZw"
+let dataReceived = []
+
+async function User() {
+    let promise = await fetch("http://192.168.60.233:3000/get_user", {username: "user1"});
+    dataReceived = await promise.json();
+    goto('/user-profile')
+}
+    
+</script>
+
+<img src="{profile_image}" alt="your profile" on:click={User}>
+
+<style>
+/* div {
+    width: 50px;
+    height: 50px;
+    box-shadow: 2px 2px 2px 2px;
+    border-radius: 100%;
+} */
+
+img {
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+    box-shadow: 2px 2px 2px 2px;
+}
+
+</style>
