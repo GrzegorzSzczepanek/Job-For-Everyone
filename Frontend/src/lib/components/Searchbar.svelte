7c138feb14060e681f03a04c5c2462858b789487
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2023-10-22 17:38:08 +0200

    remade simple searchbar

--- Frontend/src/lib/components/Searchbar.svelte
+++ Frontend/src/lib/components/Searchbar.svelte
@@ -6,15 +6,16 @@
 
     let query = "";
     let searchResult = [];
-
     async function sendQuery() {
         searchResult = await backend.get("search", { query: query });
     }
 </script>
 
-<div class="searchbar">
-    <input type="text" name="search" id="search" bind:value={query} placeholder="Wyszukaj publikacje">
-    <button type="submit" on:click={sendQuery}><Fa icon={faMagnifyingGlass} color="#222" /></button>
+<div id="searchbar">
+    <input type="text" name="search" id="search" bind:value={query} placeholder="Wyszukaj publikacje... " >
+        <span id="icon" on:click={sendQuery}>
+            <Fa icon={faMagnifyingGlass} on:click={sendQuery} size="1.6x" color="#8f9a9c"/>
+        </span>
 </div>
 
 {#each searchResult as paper}
@@ -22,5 +23,44 @@
 {/each}
 
 <style lang="scss">
+    #searchbar {
+        width: 65%;
+        position: relative;
+    }
+    #searchbar #icon{
+        position: absolute;
+        right: 24px;
+        top: 18px;
+        
+    }
+
+    input {
+        width: 100%;
+        border: none;
+        background: linear-gradient(#303133,#303133) padding-box,linear-gradient(90deg,#ed6e61,#6359e1) border-box;
+        border: 4px solid transparent;
+        border-radius: 30px;
+        text-align: left;
+        color: #f9f1ff;
+        font-weight: bold;
+        font-size: 25px;
+        padding: 10px;
+        font-family: "DM Sans";
+    }
+    input::placeholder {
+        font-weight: 600;
+        font-size: 25px;
+        color: #8f9a9c;
+        display: flex;
+        padding: 0px;
+    }
+    ::-webkit-input-placeholder {
+        text-align: left;
+        
+    }
+
+    :-moz-placeholder {
+        text-align: left;
+    }
 
 </style>
--- Frontend/src/routes/(tests)/tests/+page.svelte
+++ Frontend/src/routes/(tests)/tests/+page.svelte
@@ -1,9 +1,10 @@
 <script>
 	import Footer from "$lib/components/Footer.svelte";
 import PublicationOnProfile from "$lib/components/PublicationOnProfile.svelte";
+	import Searchbar from "$lib/components/Searchbar.svelte";
 import UserIcon from "$lib/components/UserIcon.svelte";
 
 </script>
-
-<UserIcon />
-<Footer />
+<Searchbar />
+<!-- <UserIcon />
+<Footer /> -->
