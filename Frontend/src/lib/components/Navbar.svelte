cbb555b5ab9712c446a9b68aed0c7762c8bdcf76
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2023-10-25 22:57:42 +0200

    Needed to update one thing in nav since it didn't check how it looks monitor with better resolution

--- Frontend/src/lib/components/Navbar.svelte
+++ Frontend/src/lib/components/Navbar.svelte
@@ -70,7 +70,8 @@
         border: #232325 2px solid;
     }
     nav {
-        position: relative;
+        position: absolute;
+        top: 0;
         width: 100%;
         display: flex;
         justify-content: space-between;
